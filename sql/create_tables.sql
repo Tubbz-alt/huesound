1fda42e98a524d7c436620f8d1d4a4b9ba23d792
Robert Kaye <rob@musicbrainz.org>
2012-05-22 01:00:01 -0700

    Changed the schema for use with Spotify

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,18 +4,40 @@
 
 BEGIN;
 
-CREATE TABLE color_rdio_cube (
+CREATE TABLE color_cube (
     id         SERIAL,
-    album_key  VARCHAR(16),
+    album_uri  TEXT,
     red        INTEGER,
     green      INTEGER,
     blue       INTEGER,
-    color      CUBE,
-    icon_url   TEXT,
-    embed_url  TEXT,
-    can_stream BOOLEAN
+    color      CUBE
 );
 
-CREATE UNIQUE INDEX color_rdio_cube_album_key_index ON color_rdio_cube (album_key);
+CREATE TABLE color_cube_country (
+    color      INTEGER NOT NULL, -- references color_cube.id
+    country    INTEGER NOT NULL  -- references country.id
+);
+
+CREATE TABLE country (
+    id         SERIAL,
+    code       VARCHAR(16)
+);
+
+CREATE UNIQUE INDEX color_cube_album_key_index ON color_cube (album_uri);
+CREATE UNIQUE INDEX color_cube_id_index ON color_cube (id);
+CREATE UNIQUE INDEX country_code_id_index ON country (id);
+CREATE UNIQUE INDEX country_code_index ON country (code);
+CREATE UNIQUE INDEX color_cube_country_color_index ON color_cube_country(color);
+CREATE UNIQUE INDEX color_cube_country_country_index ON color_cube_country(country);
+
+ALTER TABLE color_cube_country
+    ADD CONSTRAINT color_cube_fk_color
+    FOREIGN KEY (color)
+    REFERENCES color_cube(id);
+
+ALTER TABLE color_cube_country
+    ADD CONSTRAINT color_cube_fk_country
+    FOREIGN KEY (country)
+    REFERENCES country(id);
 
 COMMIT;
